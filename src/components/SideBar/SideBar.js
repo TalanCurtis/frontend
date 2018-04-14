d085862c073a634f8e6f765c912e00ab397ae1af
Alan Curtis <fakeemail.@email.com>
2018-04-13 18:48:33 -0600

    added some style to the link buttons.

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import menu_icon from '../../images/assets/menu-icon.png'
 
-const styles={
+// transition styles for the menu to slide and fade.
+const styles = {
     transition: 'left .5s ease-in-out, background-color .5s ease-in-out'
 }
 
@@ -11,31 +13,40 @@ class SideBar extends Component {
         this.state = {
             isOpen: false,
             menuPosition: -200,
-            bgAlpha:1
+            bgAlpha: 1
         }
     }
 
-    handleMenuTransition(){
+    handleMenuTransition() {
         console.log('handle menu Transition', this.state)
-        if(this.state.isOpen){
+        if (this.state.isOpen) {
             // set these values when menu closes
-            this.setState({menuPosition:-200, bgAlpha:this.props.scrollPercent})
-        }else{
+            this.setState({menuPosition: -200, bgAlpha: this.props.scrollPercent})
+        } else {
             // set these values when menu is opens
-            this.setState({menuPosition:0, bgAlpha:1})
+            this.setState({menuPosition: 0, bgAlpha: 1})
         }
-        this.setState({isOpen:!this.state.isOpen})
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 
     render() {
         return (
-            <div className='SideBar' style={{...styles, left:this.state.menuPosition,
-             backgroundColor:`rgba(18, 3, 36 ,${this.state.isOpen? this.state.bgAlpha : this.props.scrollPercent})`}}>
+            <div
+                className='SideBar'
+                style={{
+                ...styles,
+                left: this.state.menuPosition,
+                backgroundColor: `rgba(18, 3, 36 ,${this.state.isOpen
+                    ? this.state.bgAlpha
+                    : this.props.scrollPercent})`
+            }}>
                 {/* hidden menu links displayed when menu roles out */}
                 <div className='menuLinks'>
-                    <div className='link1'>here link 1</div>
-                    <div className='link2'>here link 2</div>
-                    <div className='link3'>here link 3</div>
+                        <Link to='/'><h5>Home</h5></Link>
+                        <Link to='/'><h5>About</h5></Link>
+                        <Link to='/'><h5>Contact</h5></Link>
                     <button onClick={() => console.log(this.props)}>Props</button>
                 </div>
 
