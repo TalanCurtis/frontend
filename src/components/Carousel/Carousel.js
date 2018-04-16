8f8b91ee005d09c38cc5c2b15af3d8ee094caf4b
Alan Curtis <fakeemail.@email.com>
2018-04-16 13:50:11 -0600

    styled carousel and card, looking good.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -11,21 +11,26 @@ class Carousel extends Component {
         super(props);
         this.state = {
             slideCount: 1,
-            slides:[0, 1, 2, 3],
-            left:0
+            slides: [],
+            left: 0
         }
+    }
+
+    componentDidMount() {
+        this.setState({slides: this.props.content.slides})
     }
 
     handleSlideBack() {
         console.log('slideBack')
-        if(this.state.slideCount <= 1){
-            // If you are at the begginng of the slides and hit back.
-            // it gets the number of slides -1 then multiplies that by how far to move to take you to the last slide.
+        if (this.state.slideCount <= 1) {
+            // If you are at the begginng of the slides and hit back. it gets the number of
+            // slides -1 then multiplies that by how far to move to take you to the last
+            // slide.
             this.setState({
                 slideCount: this.state.slides.length,
-                left: (this.state.slides.length-1) * -270
+                left: (this.state.slides.length - 1) * -270
             })
-        }else{
+        } else {
             // the amount to move is the width of the CarouselCard image plus padding.
             this.setState({
                 slideCount: this.state.slideCount - 1,
@@ -35,12 +40,9 @@ class Carousel extends Component {
     }
 
     handleSlideForeword() {
-        if(this.state.slideCount >= this.state.slides.length){
-            this.setState({
-                slideCount: 1,
-                left: 0
-            })
-        }else{
+        if (this.state.slideCount >= this.state.slides.length) {
+            this.setState({slideCount: 1, left: 0})
+        } else {
             this.setState({
                 slideCount: this.state.slideCount + 1,
                 left: this.state.left - 270
@@ -49,18 +51,31 @@ class Carousel extends Component {
     }
 
     render() {
-        const slides = this.state.slides.map((x, i) => {
-            return (<CarouselCard key={i} content={x}/>)
-        })
+        const slides = this
+            .state
+            .slides
+            .map((x, i) => {
+                return (<CarouselCard key={i} content={x}/>)
+            })
 
         return (
             <div className='Carousel'>
+                <button onClick={() => console.log(this.props.content)}>Test</button>
                 <header>
                     <div className='leftCheveron' onClick={() => this.handleSlideBack()}/>
                     <div className='rightCheveron' onClick={() => this.handleSlideForeword()}/>
-                    <h5>{this.props.content.navText.toUpperCase()}</h5>
+                    <h5>{this
+                            .props
+                            .content
+                            .navText
+                            .toUpperCase()}</h5>
                 </header>
-                <div className='slideContainer'  style={{...styles, left: this.state.left}}>
+                <div
+                    className='slideContainer'
+                    style={{
+                    ...styles,
+                    left: this.state.left
+                }}>
                     {slides}
                 </div>
             </div>
--- src/components/CarouselCard/CarouselCard.js
+++ src/components/CarouselCard/CarouselCard.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import commentImg from '../../images/assets/comment.png';
+import trippleDotImg from '../../images/assets/tripple-dot.png';
+import heartImg from '../../images/assets/heart.png';
 
 function CarouselCard(props) {
     return (
         <div className='CarouselCard'>
-            <h5>
-                Card {props.content}
-            </h5>
+            {/* <img src="../images/assets/image1.jpg" alt=""/> */}
+            {/* <img src="../../images/assets/image1.jpg" alt=""/> */}
+            {/* <img src={`url("../images/assets/image1.jpg")`} alt=""/> */}
+            <div className='mainImage'>
+                <div className='likes'>
+                    <img className='heartImg' src={heartImg} alt=""/>
+                    <p>382</p>
+                </div>
+            </div>
+            <div className='info'>
+                <h5>{props.content.title}</h5>
+                <div>
+                    <p>{props.content.description}</p>
+                </div>
+            </div>
+            <footer>
+                <div className='footerInfo'>
+                    <img className='commentImg' src={commentImg} alt=""/>
+                    <p>475 Comments</p>
+                </div>
+                <img className='trippleDotImg' src={trippleDotImg} alt=""/>
+            </footer>
         </div>
     )
 }
