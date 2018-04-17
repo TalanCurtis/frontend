3d05d51cd474b57da1ff24ce65dfa38917b16212
Alan Curtis <fakeemail.@email.com>
2018-04-17 13:59:58 -0600

    added columns and pictu paragrapsh to landing .

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 // import components
 import SideBar from '../../components/SideBar/SideBar'
@@ -30,12 +31,26 @@ class Landing extends Component {
     }
 
     render() {
+        let articalColumns = _.isEmpty(this.state.sliderArticles)?null: this.state.sliderArticles.columns.map((x,i)=>{return(
+            <div key={i} className='column'>
+                <h4>{x.title}</h4>
+                <p>{x.description}</p>
+            </div>
+        )})
         return (
             <div className='Landing'>
                 <SideBar navText={this.state.intro.navText}/>
                 <Intro content={this.state.intro}/>
                 <Carousel content={this.state.sliderPictures} />
+                <div className='pictureParagraphs'>
+                    <p className='p1'>{this.state.sliderPictures.para1}</p>
+                    <p className='p2'>{this.state.sliderPictures.para2}</p>
+                </div>
                 <Carousel content={this.state.sliderArticles} />
+                <div className='articalColumns'>
+                    <div className='blank'></div>
+                    {articalColumns}
+                </div>
             </div>
         )
     }
