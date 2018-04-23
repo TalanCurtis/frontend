e397b95f75ae6a72206295ccc1e6c8bae47be5d4
Alan Curtis <fakeemail.@email.com>
2018-04-23 17:13:17 -0600

    transitions kinda working need to tighten up.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,40 @@ import Details from './views/Details/Details';
 import SideBar from './components/SideBar/SideBar';
 import Test from './views/Tests/Fonts';
 import {Switch, Route} from 'react-router';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+
+const PageFade = (props) => (
+    <CSSTransition 
+      {...props}
+      classNames="fadeTranslate"
+      timeout={1000}
+      mountOnEnter={true}
+      unmountOnExit={true}
+    />
+  )
 
 class App extends Component {
+
+
 
     render() {
         return (
             <div className="App">
                 <div className="app-container">
                     <SideBar />
-                    <Switch>
+                    <Route render={(location)=>(
+                        <TransitionGroup>
+                        <PageFade key={location.location.key}>
+                    <Switch location={location.location} >
+                    {console.log('Location', location)}
                         <Route exact path="/" component={Landing} />
                         <Route path="/Details/:type/:id" component={Details} />
                         <Route path="/test" component={Test} />
                     </Switch>
+                    </PageFade>
+                    </TransitionGroup>
+                        )}/>
                 </div>
             </div>
         );
